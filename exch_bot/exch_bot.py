9aa74abf8be42cbd76ff531cb2910570fe0506f8
David Khurtsidze <raingts@mail.ru>
2023-02-19 17:11:38 +0300

    fix: text formatting in HTML format, some messages renamed

--- exch_bot/exch_bot.py
+++ exch_bot/exch_bot.py
@@ -2,22 +2,23 @@ import telebot
 from cfg import TOKEN, currencies_
 from extensions import APIException, ExchangeConverter
 
-bot = telebot.TeleBot(TOKEN, parse_mode="Markdown")
+bot = telebot.TeleBot(TOKEN, parse_mode="HTML")
 
 
 @bot.message_handler(commands=["start", "help"])
 def start_help(message: telebot.types.Message):
-    text = "Что бы начать работу, введите команду в формате:\n\n< *Валюта А* >\
-           < *Валюта Б* >\
-           < *Количество Б в А* >\n\n*Валюта А* - Название валюты, цену которой хотите узнать" \
-           "\n*Валюта Б* - Название валюты, в которой надо узнать цену первой валюты" \
-           "\n*Количество Б в А* - Количество первой валюты\n\nУвидеть список всех доступных валют: /values"
+    text = "Что бы начать работу, введите команду в формате:\n\n( <b>Валюта А</b> )\
+           ( <b>Валюта Б</b> )\
+           ( <b>Количество Б в А</b> )\n\n<b>Валюта А</b> - Название валюты, цену которой хотите узнать" \
+           "\n<b>Валюта Б</b> - Название валюты, в которой надо узнать цену первой валюты" \
+           "\n<b>Количество Б в А</b> - Количество первой валюты\n\n<b>Увидеть список всех доступных валют:" \
+           " /exrates</b>"
     bot.reply_to(message, text)
 
 
-@bot.message_handler(commands=["values"])
+@bot.message_handler(commands=["exrates"])
 def currencies(message: telebot.types.Message):
-    text = "Доступные валюты:\n"
+    text = "<b>Доступные валюты</b>"
     for key in currencies_.keys():
         text = "\n".join((text, key))
     bot.reply_to(message, text)
