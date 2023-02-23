e43533d459b70d1a38432568a21eb5ccf156b081
David Khurtsidze <raingts@mail.ru>
2023-02-23 19:58:19 +0300

    fix: changed quotes

--- exch_bot/cfg.py
+++ exch_bot/cfg.py
@@ -1,11 +1,11 @@
-TOKEN = ""
+TOKEN = "5973029943:AAFuu2G2sZHjYVAtV2Xxgu9jzKR_r0_3TKc"
 
 currencies_ = {
-    'доллар': 'USD',
-    'евро': 'EUR',
-    'рубль': 'RUB'
+    "доллар": "USD",
+    "евро": "EUR",
+    "рубль": "RUB"
 }
 
 headers = {
-    "apikey": ""
+    "apikey": "6kPFJ11Dwx3rwCw33lXQFVOnnZ3ReGvN"
 }
